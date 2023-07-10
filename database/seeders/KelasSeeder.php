2e8a80666a2c5c528d3e53f2afce43cd62d2d7a9
xenrath <xenrath89@gmail.com>
2023-07-10 20:03:03 +0700

    kelas seeder

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -18,27 +18,51 @@ class KelasSeeder extends Seeder
 
         $kelas = [
             [
-                'nama' => '1',
+                'nama' => '1A',
                 'kelas' => '1',
             ],
             [
-                'nama' => '2',
+                'nama' => '1B',
+                'kelas' => '1',
+            ],
+            [
+                'nama' => '2A',
                 'kelas' => '2',
             ],
             [
-                'nama' => '3',
+                'nama' => '2B',
+                'kelas' => '2',
+            ],
+            [
+                'nama' => '3A',
                 'kelas' => '3',
             ],
             [
-                'nama' => '4',
+                'nama' => '3B',
+                'kelas' => '3',
+            ],
+            [
+                'nama' => '4A',
                 'kelas' => '4',
             ],
             [
-                'nama' => '5',
+                'nama' => '4B',
+                'kelas' => '4',
+            ],
+            [
+                'nama' => '5A',
                 'kelas' => '5',
             ],
             [
-                'nama' => '6',
+                'nama' => '5B',
+                'kelas' => '5',
+            ],
+            [
+                'nama' => '6A',
+                'kelas' => '6',
+            ],
+            [
+                'nama' => '6B',
                 'kelas' => '6',
             ],
         ];
